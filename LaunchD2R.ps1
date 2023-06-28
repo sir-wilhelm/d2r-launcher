bd7b1c14c8b085518e27401b9c5de73e2759d716
sir-wilhelm <sir-wilhelm@users.noreply.github.com>
2023-06-28 17:40:21 -0500

    Auto format.

--- LaunchD2R.ps1
+++ LaunchD2R.ps1
@@ -1,13 +1,13 @@
 #Requires -Modules CredentialManager
 
 param(
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$Account,
 
     [ValidateSet("direct", "launcher")]
     [string]$LaunchMode = "direct",
 
-    [ValidateSet("eu","us","kr")]
+    [ValidateSet("eu", "us", "kr")]
     [string]$Region = "us",
 
     [ValidateSet("1", "2", "3", "4", "5")]
@@ -17,17 +17,16 @@ param(
 Import-Module CredentialManager
 
 $credential = Get-StoredCredential -Target "b.net/$Account"
-if(!$credential) {
+if (!$credential) {
     "Account not found, make sure it was saved with "
     exit 1
 }
 
 $d2rPath = "D:\Battle.net\Diablo II Resurrected - $ClientNumber"
 
-if($LaunchMode -eq "direct")
-{
-	& "$d2rPath\D2R.exe" -address "$Region.actual.battle.net" -username $credential.UserName -password $credential.GetNetworkCredential().Password
+if ($LaunchMode -eq "direct") {
+    & "$d2rPath\D2R.exe" -address "$Region.actual.battle.net" -username $credential.UserName -password $credential.GetNetworkCredential().Password
 }
-else{
+else {
     & "$d2rPath\Diablo II Resurrected Launcher.exe"
 }

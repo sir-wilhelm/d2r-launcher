3dc0e523f4478cc388043ca035ef495effdbfc1f
sir-wilhelm <sir-wilhelm@users.noreply.github.com>
2024-05-27 11:19:56 -0500

    Support up to 8 clients.

--- LaunchD2R.ps1
+++ LaunchD2R.ps1
@@ -7,7 +7,7 @@ param(
     [string]$LaunchMode = "direct",
     [ValidateSet("eu", "us", "kr")]
     [string]$Region = "us",
-    [ValidateSet("1", "2", "3", "4", "5")]
+    [ValidateSet("1", "2", "3", "4", "5", "6", "7", "8")]
     [string]$ClientNumber = "2"
 )
 
